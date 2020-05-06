6137b2c2aa945e3bf7ee3372114e17dfaf6d60b3
Invalid-Argument <63424529+Invalid-Argument@users.noreply.github.com>
2020-05-06 01:39:18 -0500

    Update Generador_Contraseñas.py
    Programa sencillo que genera contraseñas

--- Códigos/Código1/Generador_Contraseñas.py
+++ Códigos/Código1/Generador_Contraseñas.py
@@ -1,1 +1,15 @@
+#InvalidArgument
+import random
+import string
 
+print("Bienvenido a mi generador de contraseñas")
+num_carac=int(input("Ingrese el número de caracteres que desee que contenga su contraseña: "))
+
+def generandocontra (num_carac):
+  pass=""
+  for i in range(num_carac):
+    char=random.choice(string.ascii_lowercase+string.ascii_uppercase+string.digits)
+    pass = pass + char
+  print(f"La contraseña que se creó es: {pass}")
+
+generandocontra(num_carac)
