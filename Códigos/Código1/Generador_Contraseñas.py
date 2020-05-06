fea1c6d696453a77aab8cc45823df8f55005b77f
Invalid-Argument <63424529+Invalid-Argument@users.noreply.github.com>
2020-05-06 01:55:11 -0500

    Update Generador_Contraseñas.py

--- Códigos/Código1/Generador_Contraseñas.py
+++ Códigos/Código1/Generador_Contraseñas.py
@@ -3,13 +3,13 @@ import random
 import string
 
 print("Bienvenido a mi generador de contraseñas")
-num_carac=int(input("Ingrese el número de caracteres que desee que contenga su contraseña: "))
+num_carac = int(input("Ingrese el número de caracteres que desee que contenga su contraseña: "))
 
 def generandocontra (num_carac):
-  pass=""
+  password = " "
   for i in range(num_carac):
     char=random.choice(string.ascii_lowercase+string.ascii_uppercase+string.digits)
-    pass = pass + char
-  print(f"La contraseña que se creó es: {pass}")
+    password = password + char
+  print(f"La contraseña que se creó es: {password}")
 
 generandocontra(num_carac)
